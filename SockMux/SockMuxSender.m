0a37cc1a7e42072be6e127db51e9c52d13a7b8b5
Daniel Mack <zonque@gmail.com>
2011-12-09 15:31:51 +0100

    fixed licence copy-paste

--- SockMux/SockMuxSender.m
+++ SockMux/SockMuxSender.m
@@ -3,7 +3,7 @@
  
  Copyright 2011 Daniel Mack <sockmux@zonque.org>
  
- PulseAudioOSX is free software; you can redistribute it and/or modify
+ SockMux is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License (LGPL) as
  published by the Free Software Foundation; either version 2.1 of the
  License, or (at your option) any later version.
